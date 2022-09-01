1862940dea83413f6b788b1631f247eb0b2f5525
Amélie Gruel <ameliegruel@free.fr>
2022-09-01 15:27:17 +0200

    add one example

--- BinaryFoveation/loadDVSdata.py
+++ BinaryFoveation/loadDVSdata.py
@@ -1,22 +1,23 @@
-from DVSgesture_dataset import *
+from DVSgesture_dataset import DVSGesture_dataset
+dir = '/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/'
 
 # load data from test dataset
 d = DVSGesture_dataset(
-    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    repertory=dir, 
     type_data= 'test',
     spatial_divider=1
 )
 
 # load data reduced using cubic
 d = DVSGesture_dataset(
-    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    repertory=dir, 
     type_data= 'reduce',
     method='cubic'
 )
 
 # load 40% of data reduced using eventcount
 d = DVSGesture_dataset(
-    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    repertory=dir, 
     type_data= 'reduce',
     method='eventcount', 
     structural_divider=40
@@ -24,7 +25,7 @@ d = DVSGesture_dataset(
 
 # load data foveated using linear, with ROI obtained on data reduced using cubic
 d = DVSGesture_dataset(
-    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    repertory=dir, 
     type_data= 'fovea',
     method='linear',
     roi_method='cubic'
@@ -32,11 +33,18 @@ d = DVSGesture_dataset(
 
 # load 5% of data foveated using cubic, with ROI obtained on data reduced using funnelling
 d = DVSGesture_dataset(
-    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    repertory=dir, 
     type_data= 'fovea',
     method='cubic',
     roi_method='funnelling',
     structural_divider=5
+)
+
+# load data foveated using eventcount, with ROI obtained on whole data (no reduction)
+d = DVSGesture_dataset(
+    repertory=dir, 
+    type_data= 'fovea',
+    method='eventcount'
 )
 
 # read first event of 99 first samples
