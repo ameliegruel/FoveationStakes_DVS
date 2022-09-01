dfbdcaa391a820e381f1439b3fe87415893a006a
Amélie Gruel <ameliegruel@free.fr>
2022-09-01 14:42:24 +0200

    example script on how to use DVSgesture_dataset tonic wrapper

---
+++ BinaryFoveation/loadDVSdata.py
@@ -0,0 +1,46 @@
+from DVSgesture_dataset import *
+
+# load data from test dataset
+d = DVSGesture_dataset(
+    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    type_data= 'test',
+    spatial_divider=1
+)
+
+# load data reduced using cubic
+d = DVSGesture_dataset(
+    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    type_data= 'reduce',
+    method='cubic'
+)
+
+# load 40% of data reduced using eventcount
+d = DVSGesture_dataset(
+    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    type_data= 'reduce',
+    method='eventcount', 
+    structural_divider=40
+)
+
+# load data foveated using linear, with ROI obtained on data reduced using cubic
+d = DVSGesture_dataset(
+    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    type_data= 'fovea',
+    method='linear',
+    roi_method='cubic'
+)
+
+# load 5% of data foveated using cubic, with ROI obtained on data reduced using funnelling
+d = DVSGesture_dataset(
+    repertory='/home/amelie/Scripts/Data/DVS128Gesture/tonic_data/ZIP files/', 
+    type_data= 'fovea',
+    method='cubic',
+    roi_method='funnelling',
+    structural_divider=5
+)
+
+# read first event of 99 first samples
+for i, (events, target) in enumerate(iter(d)):
+    print(i, target, events[0])
+    if i > 99:
+        break
